8c86e90262368c67e3b5a10f380c82981bfe6b05
Iamneo <techadmin@iamneo.ai>
2023-08-04 09:27:48 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { NewFoodComponent } from './component/new-food/new-food.component';
+import { FoodSearchPipe } from './component/new-food/food-search.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewFoodComponent,
+    FoodSearchPipe
   ],
   imports: [
     BrowserModule,
